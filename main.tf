a0e21ac28a191795b468f1fb7772ef8c7fad2bb9
Matt Boston <mj.boston02@gmail.com>
2023-05-11 22:28:36 +0100

    ModificationsS3 ACL policies and public access

--- main.tf
+++ main.tf
@@ -25,7 +25,28 @@ resource "aws_s3_bucket" "website" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "website_ownership" {
+  bucket = aws_s3_bucket.website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_public_access" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "bucket-acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website_ownership,
+    aws_s3_bucket_public_access_block.website_public_access,
+  ]
+
   bucket = aws_s3_bucket.website.id
   acl    = "public-read"
 }
@@ -42,22 +63,26 @@ resource "aws_s3_bucket_website_configuration" "s3_website_config" {
 
 resource "aws_s3_bucket_policy" "s3_website_policy" {
   bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${aws_s3_bucket.website.id}/*"
-        }
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
     ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.website.id}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+  }
 }
-POLICY
-}
+
 
 # --------------------------------------------------------------------------
 # ACM Certificate
